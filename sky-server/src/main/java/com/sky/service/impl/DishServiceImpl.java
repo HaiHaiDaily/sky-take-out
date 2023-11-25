1d9c6260af0dabd8e4d89f8b7f9bfc6fef942809
HaiHai <2362349147@qq.com>
2023-11-26 01:43:10 +0800

    菜品修改业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -18,6 +18,7 @@ import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -115,6 +116,55 @@ public class DishServiceImpl implements DishService {
 
         //批量删除菜品关联的口味数据
         dishFlavorMapper.deleteByDishIds(ids);
+
+    }
+
+    /**
+     * 根据id查询菜品
+     * @param id
+     * @return
+     */
+    public DishVO getByIdWithFlavor(Long id) {
+        //根据id查询菜品数据
+        Dish dish = dishMapper.getById(id);
+
+        //根据菜品id查询口味数据
+        List<DishFlavor> dishFlavors =dishFlavorMapper.getByDishId(id);
+
+        //将两个数据封装到VO
+        DishVO dishVO= new DishVO();
+        BeanUtils.copyProperties(dish,dishVO);
+        dishVO.setFlavors(dishFlavors);
+
+        return dishVO;
+    }
+
+    /**
+     * 根据id修改菜品基本信息和对应的口味信息
+     * @param dishVO
+     */
+    public void updateWithFlavor(DishVO dishVO) {
+        Dish dish=new Dish();
+        BeanUtils.copyProperties(dishVO,dish);
+
+        //根据id修改菜品基本数据
+        dishMapper.update(dish);
+
+        //删除所有对应的口味数据
+        dishFlavorMapper.deleteByDishId(dishVO.getId());
+
+        //获取口味数组并且给每个口味实体添加菜品id
+        List<DishFlavor> flavors = dishVO.getFlavors();
+        //可能会有新添加的口味，要重新给他们添加菜品id
+        flavors.forEach(dishFlavor -> {
+            dishFlavor.setDishId(dishVO.getId());
+        });
+
+        //口味不为空时
+        if(flavors!=null && flavors.size()>0){
+            //向口味插入n条数据
+            dishFlavorMapper.insertBatch(flavors);
+        }
 
     }
 }
